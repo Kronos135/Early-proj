ec1ae0b2aaba634fb9fedb0924d36bfa13631cc7
Kronos135 <thedarkestmaster@gmail.com>
2019-05-29 22:45:05 +0300

    First test

---
+++ SysOverview/logic.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+
+namespace SysOverview
+{
+    class logic
+    {
+        public static string GetComponent(string hwclass, string syntax)
+        {
+
+            ManagementObjectSearcher mos = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM " + hwclass);
+            if (mos == null)
+                return "-1";
+            foreach (ManagementObject item in mos.Get())
+            {
+                return Convert.ToString(item[syntax]);
+            }
+            return "";
+        }
+    }
+}
