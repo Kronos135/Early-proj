33ade63191c05e0efc35d3ad8e2714562ac0c1fc
Kronos135 <thedarkestmaster@gmail.com>
2019-06-03 21:58:49 +0300

    localize

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(Program.XO(Console.ReadLine()));
+            Console.ReadLine();
+
+        }
+        public static bool XO(string input)
+        {
+            bool x = true;
+            bool o = true;
+            for(int i=0; i<input.Length ;i++)
+            {
+                switch (input[i])
+                {
+                    case 'o':
+                    case 'O':
+                        o = !o;
+                        Console.WriteLine("1o");
+                        break;
+                    case 'x':
+                    case 'X':
+                        x = !x;
+                        Console.WriteLine("1x");
+                        break;
+                }
+            }
+            if (x == true && o == true)
+                return true;
+            else
+                return false;
+        }
+    }
+}
--- SysOverview/Form1.cs
+++ SysOverview/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,16 +16,74 @@ namespace SysOverview
         public Form1()
         {
             InitializeComponent();
-            Processor.Text = logic.GetComponent("Win32_Processor", "Name");
-            Video.Text = logic.GetComponent("Win32_VideoController", "Name");
-            Baseboard.Text = logic.GetComponent("Win32_BaseBoard", "Manufacturer") + " " + logic.GetComponent("Win32_BaseBoard", "Product");
-            Ram.Text = logic.GetPhysicalMemory();
+            iProcessor.Text = logic.GetComponent("Win32_Processor", "Name");
+            iVideo.Text = logic.GetComponent("Win32_VideoController", "Name");
+            iBaseboard.Text = logic.GetComponent("Win32_BaseBoard", "Manufacturer") + " " + logic.GetComponent("Win32_BaseBoard", "Product");
+            iRam.Text = logic.GetPhysicalMemory();
         }
 
-        private void Label1_Click(object sender, EventArgs e)
+
+
+        private void СохранитьФайлToolStripMenuItem_Click(object sender, EventArgs e)
         {
 
         }
+
+        private void EnglishToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ChangeLanguage("en");
+        }
+        private void РусскийToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            ChangeLanguage("");
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            //this.Close();
+            Application.Exit();
+
+        }
+
+        private void Info_Click(object sender, EventArgs e)
+        {
+            var F2 = new Form2();
+            F2.ShowDialog();
+        }
+
+        private void ChangeLanguage(string newLanguageString)
+        {
+            var resources = new ComponentResourceManager(typeof(Form1));
+
+            CultureInfo newCultureInfo = new CultureInfo(newLanguageString);
+
+            //Применяем для каждого контрола на форме новую культуру
+            foreach (Control c in this.Controls)
+            { 
+                if(c.Name[0]!='i')
+                    resources.ApplyResources(c, c.Name, newCultureInfo);
+            }
+
+            //Отдельно меняем для тайтла самой формы локализацию
+            resources.ApplyResources(this, "$this", newCultureInfo);
+
+            resources.ApplyResources(this.MainMenu, "MainMenu", newCultureInfo);
+
+            //Для элементов статус стрипа устанавливаем также установки локализации
+            foreach (var item in this.MainMenu.DropDownItems.Cast<ToolStripItem>())
+            {
+                resources.ApplyResources(item, item.Name, newCultureInfo);
+            }
+            /*
+                        //Устанавливаем текст на кнопке, которая была изображена на скриншоте раньше название локализации
+                        TSDD_Language.Text = newCultureInfo.NativeName;
+
+                        //Устанавливаем флаг на активной локализации (см. функцию ниже)
+                        SetCurrenLanguageButtonChecked();
+                        */
+        }
+
+ 
     }
 
 }
